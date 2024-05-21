e9178d3f1b969761553e45bbfc5b027963af148b
Aditya Gupta <146000890+Adii-7595@users.noreply.github.com>
2024-05-21 10:02:37 +0530

    added files

---
+++ Javascript/14-StringMethod.js
@@ -0,0 +1,29 @@
+//method : action that can be performed on object
+//format : stringName.method()
+
+
+//trim method: trim whitespace from both ends of strings and returns a new one- no whitespace will be remove from in between strings
+
+let msg= "  ads  ";
+console.log(msg.trim());
+//trim create a new string with unavailable whitespace.
+//thats why string in JS  is immutable
+
+//toUpperCase(), toLowerCase()
+
+let a = "adiTYa"
+let a1 = a.toUpperCase()
+let a2 = a.toLowerCase()
+
+console.log(a1,"\n"+a2)
+
+//string method with arguments
+
+//Argument is a some value that we pass to the method
+//indexOf
+
+console.log(a.indexOf("i"))
+console.log(a.indexOf("adi"))
+console.log(a.indexOf("TYa"))
+
+//
---
+++ Javascript/2-script.js
@@ -0,0 +1,18 @@
+console.log("Aditya Bro!");
+let a=10;
+let b=12;
+console.log("Sum is:", a+b);
+diff=b-a;
+console.log("diff is:", diff);
+//comment in js
+
+// alert("hey wait u r fucked up!")
+// console.log("this is a simple log")
+// console.error("this is an error msg")
+// console.warn("this is a warning msg")
+
+// let firstName = prompt("enter ur first name")
+// let lastname = prompt("enter ur ;ast name")
+
+// let msg = `Welcome ${firstName} ${lastname}!`;
+// alert(msg);
